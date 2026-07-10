a44b5d9d52992170dba9b8840d2b4f8e046b3d7b
powemetal <francisbvrt@gmail.com>
2026-07-09 23:33:44 -0400

    [Feat] Ajout de la popupbox pour confirmation de capture et relachement de pokemon

--- src/main/java/org/example/pokedex_francis_pascale/controller/PokemonMainController.java
+++ src/main/java/org/example/pokedex_francis_pascale/controller/PokemonMainController.java
@@ -9,6 +9,7 @@ import org.example.pokedex_francis_pascale.exceptions.PokemonIntrouvableExceptio
 import org.example.pokedex_francis_pascale.modele.Pokemon;
 import org.example.pokedex_francis_pascale.modele.PokemonDAO;
 import org.example.pokedex_francis_pascale.service.PokemonApiService;
+import org.example.pokedex_francis_pascale.utils.ConfirmationBox;
 import org.example.pokedex_francis_pascale.view.PokemonViewFX;
 import javafx.scene.image.Image;
 
@@ -93,6 +94,9 @@ public class PokemonMainController {
         if (pokemonTrouverListe == null) {
             return;
         }
+        if (!new ConfirmationBox().boite("Capturer ce Pokémon?", "Confirmation")) {
+            return;
+        }
         try {
             dao.sauvegarder(pokemonTrouverListe);
             refreshList();
@@ -116,6 +120,9 @@ public class PokemonMainController {
             }
         }
         if (selectionList == null) {
+            return;
+        }
+        if (!new ConfirmationBox().boite("Voulez vous vraiment relacher ce Pokémon ?", "Confirmation")) {
             return;
         }
         try{
